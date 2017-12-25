b97c43696beb9a37bc57d9a579ae7ad0895f586e
BabylonBwoy <junglecrew@gmail.com>
2017-12-25 09:00:42 +0200

    auth flip

--- source/pages/index/index.js
+++ source/pages/index/index.js
@@ -7,4 +7,25 @@ import '../../style/fonts.scss';
 // import '../../img/sprite.svg';
 
 console.log('in index.js');
-// console.log($);
+
+var authBtnTrigger = document.querySelector('.auth-button');
+var flip = document.querySelector('.auth');
+var form = document.querySelector('.container');
+
+
+function rotate(event) {
+  flip.classList.add('rotated');
+  authBtnTrigger.style.opacity ='0';
+  authBtnTrigger.style.cursor ='default';
+}
+
+window.onclick = function(event) {
+  var target = event.target; // где был клик?
+  if (target == form ) {
+    flip.classList.remove('rotated');
+    authBtnTrigger.style.opacity ='1';
+    authBtnTrigger.style.cursor ='pointer';
+  }
+};
+
+authBtnTrigger.addEventListener('click', rotate);
